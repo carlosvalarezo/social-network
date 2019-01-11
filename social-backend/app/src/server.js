5647a727f6dc007002f87901691a37e5eedfec83
carlos-valarezo-loaiza <carlos-valarezo-loaiza@carlos-valarezo-loaiza.local>
2019-01-11 18:37:21 -0500

    first commit

---
+++ social-backend/app/src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => response.send('Hello world outside'));
+
+const port = process.env.PORT || 5000;
+ 
+app.listen(port, ()=>console.log(`Server on port ${port}`));
