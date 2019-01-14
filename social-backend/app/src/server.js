5db1a90788ccedb2fe2a379b178bf6ef180a687f
carlos-valarezo-loaiza <carlos-valarezo-loaiza@carlos-valarezo-loaiza.local>
2019-01-14 15:48:10 -0500

    [CV] copies the app to the container

--- social-backend/app/src/server.js
+++ social-backend/app/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => response.send('Hello world outside'));
+app.get('/', (request, response) => response.send('Hello world inside 2'));
 
 const port = process.env.PORT || 5000;
  
