43058d91c00ed7db41fbc191db544b637fe66bda
carlos-valarezo-loaiza <carlos.valarezo@gmail.com>
2019-03-08 19:00:38 -0500

    [CV] adds volume directive to frontend image

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker image prune -f
 docker-compose stop
 docker image rm -f social-frontend social-backend social-db
 docker container rm -f social-frontend 
 docker container rm -f social-backend 
 docker container rm -f social-db
+docker image prune -f
 docker-compose up --build
