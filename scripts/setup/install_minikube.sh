c5987dd8e86ccafa06ea412d08d88e067f5bb4a7
carlos-valarezo-loaiza <carlos.valarezo@gmail.com>
2019-03-17 03:52:39 -0500

    [CV] adds script to install and start minikube on mac

---
+++ scripts/setup/install_minikube.sh
@@ -0,0 +1,6 @@
+brew install --HEAD hyperkit
+brew install docker-machine-driver-hyperkit
+sudo chown root:wheel /usr/local/opt/docker-machine-driver-hyperkit/bin/docker-machine-driver-hyperkit
+sudo chmod u+s /usr/local/opt/docker-machine-driver-hyperkit/bin/docker-machine-driver-hyperkit
+brew cask install minikube
+minikube start --vm-driver=hyperkit
