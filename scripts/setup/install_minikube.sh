690cd9fd810263771e84eceeff72cbe91550d82c
carlos-valarezo-loaiza <carlos.valarezo@gmail.com>
2019-03-17 03:54:03 -0500

    [CV] sets hyperkit as default vm for minikube

--- scripts/setup/install_minikube.sh
+++ scripts/setup/install_minikube.sh
@@ -3,4 +3,5 @@ brew install docker-machine-driver-hyperkit
 sudo chown root:wheel /usr/local/opt/docker-machine-driver-hyperkit/bin/docker-machine-driver-hyperkit
 sudo chmod u+s /usr/local/opt/docker-machine-driver-hyperkit/bin/docker-machine-driver-hyperkit
 brew cask install minikube
-minikube start --vm-driver=hyperkit
+minikube config set vm-driver hyperkit
+minikube start
